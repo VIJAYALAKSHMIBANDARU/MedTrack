121a507dd21fc1a32a5a156817841ee65de51af9
Vijayalakshmi <165303846+VIJAYALAKSHMIBANDARU@users.noreply.github.com>
2026-02-01 22:36:24 +0530

    preprocessing

--- ml/explore_data.py
+++ ml/explore_data.py
@@ -3,7 +3,14 @@ print("SCRIPT STARTED")
 import pandas as pd
 
 file_path = "../data/healthcare_noshows_appt.csv"
+
 df = pd.read_csv(file_path)
 
+print("DATA LOADED SUCCESSFULLY")
+print("First 5 rows:")
 print(df.head())
+
+print("\nDATA INFO:")
 print(df.info())
+
+print("SCRIPT FINISHED")
